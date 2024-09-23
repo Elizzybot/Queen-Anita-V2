6d4e49d3a37a7df7dda71ecb66909173848e13e1
Elizzybot <elijahchinecheremonah@gmail.com>
2024-09-23 13:03:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-//#ENJOY
+we//#ENJOY
 const fs = require("fs-extra");
 if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
@@ -19,10 +19,10 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3AD
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2347038494997,2349039727490";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "teue";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -42,7 +42,7 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUhaNjZqT1ZrQmZEK3JWdjhDemhkRm5QaG1iNG1FbnQ0NEtxbFVsTkMzYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVXhGNWJGWkZxTWtrWE5xektwSkw3bGpQS1p4OGJ3QzNGcnlhV1RlQisyMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNRERRdGE3T1lUZXpneWk4T3h0QmVmNkVreEJTdjJYZWlSWUVwMXA2bTNjPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFMDA3MjBtRzNvODNDdTlIZFpGcCtsSTJVbENsdnJjNUFxVmlTeElML1VjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImlNMkwrdzR3ajA1ajZVc1M4RkViUHg5NFhlSFc0RGFWMnVnSitNVVA0bVE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndZbzRKT0NiUEs3QTJCYlJldEtwVXRONVRTYVIzRXZqaVFOYzJ0UE1ieEU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNEw4ZTd0dVJjYTNhOWIvMGRQL1BzRFRvd3daT3pPdEVXN3k1OUJ1dzVYWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNU5WV0s3K1cxL0VPZzUrb0tEcEx5emZYNnkwL2swQ0VpOGxqYUxNa1hGZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZIRTlTUGc0ZzhCOCtjaFJETTUyRUVYemIyakNTai8yWktldmZLL3dqSFlrTUNSRTZtOTBxbmh2N3d0cEV0OHBGenhJU0REcVlyNjBtTCs2dnFWUGhnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTAsImFkdlNlY3JldEtleSI6IlZhb1czNFM3ZTFaU1FYbThRNlNENEhUWTVyRUM1L25BUnZDd3FIRWI2TDQ9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IkI1NUpaWjJTUjItdEdaamM3S1RrOUEiLCJwaG9uZUlkIjoiYjE4N2ZjNTMtZjBjYi00Yzk5LWIyNTEtOTM1ZThjM2U3ZmYzIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ii9VV3o3NXdpVXdOeWtEMzQ4cGpTZW04L0RyQT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJmNDNWcEJiUC9kT292dkRWS2hMYk5Vais4WTg9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiU0NQWlhRVlAiLCJtZSI6eyJpZCI6IjIzNDkwMzk3Mjc0OTA6NDNAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ01PenI3b0dFTE93eGJjR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlBrSHBreVZ1U202UVhIcklvRng4R3JJRmV3OTkyVjhPQ2xURmluM0tvbXc9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImpRK3lwSWtvZVBuOENHWllvTXR4YjB3YVJsNjFVOVVqTis5UEtuK1BUbUxIT0hsZkI0d0NwZjAzeUF0VTIxcHMwTVlYb1hWVzRVVUUwb0ZBTEd4ekFRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJCSWJlazZSQlNSUG9RWkU1NFRNTVA0ZXU0eDFFclN1eHBBelhEbUR2dXBCTUR2SU1zOUh1SVFlQVljdDR6UmhvUGZKakhEM0E0RjdOd24raW9xTDhqQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDkwMzk3Mjc0OTA6NDNAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVDVCNlpNbGJrcHVrRng2eUtCY2ZCcXlCWHNQZmRsZkRncFV4WXA5eXFKcyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyNzA5MjgwMCwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFMRVcifQ=="
 module.exports = {
   menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
   botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  ownername: process.env.OWNER_NAME || "dark brothes",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
